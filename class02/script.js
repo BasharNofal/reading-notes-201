d547bbd385bef2b0aa8f70c69c2b13218b224bf9
bashar <bashar.nofal1997@gmail.com>
2020-12-28 14:15:47 +0200

    added the files that im gonna be working on

---
+++ class02/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var name = prompt("what is your name ?");
+alert("Hello! " + name );
+alert("I like your name, so let us play a game, and regardless of who is gonna win, I am gonna show a small Bro-hackers trick");
+
+var age= prompt("am i 40 years old ? (y/n)");
+if(toLowerCase(age)==="y") {
+    alert("that is correct, i mean if was that old i would not be here");
+} else{
+    
+}
